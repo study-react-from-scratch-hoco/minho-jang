a31609e7e66e664f228507d942e1884ead74d954
miinhho <mjang6988@gmail.com>
2025-06-11 21:40:25 +0900

    refactor: AppState 의 불필요한 getter/setter 제거

--- lib/state.ts
+++ lib/state.ts
@@ -1,26 +1,10 @@
 class AppState {
-  private appState: any[];
-  private appStateIndex: number;
+  state: any[];
+  index: number;
 
   constructor() {
-    this.appState = [];
-    this.appStateIndex = 0;
-  }
-
-  get index() {
-    return this.appStateIndex;
-  }
-
-  set index(value: number) {
-    this.appStateIndex = value;
-  }
-
-  get state() {
-    return this.appState;
-  }
-
-  set state(value: any[]) {
-    this.appState = value;
+    this.state = [];
+    this.index = 0;
   }
 }
 
